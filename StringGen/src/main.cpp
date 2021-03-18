9f9477fc12e1cb586874c5885408b16adf6bd15e
Paul Rawcliffe <jpr999@gmail.com>
2021-03-18 12:44:11 +0000

    added timing

--- StringGen/src/main.cpp
+++ StringGen/src/main.cpp
@@ -8,6 +8,8 @@
 #include <string>
 
 #include <fmt/printf.h>
+#include <fmt/chrono.h>
+
 #include "include/inc.h"
 
 std::string generate(size_t max_length);
@@ -17,31 +19,37 @@ int main()
     std::map<std::string,std::string> m;
     std::vector<std::string> v;
 
-    while(m.size() != 2000)
+    auto start = std::chrono::high_resolution_clock::now();
+    while(m.size() != 10000)
     {
         std::string key(generate(50));
         m.insert(std::pair(key,generate(3000)));
         v.push_back(key);
 
     }
-
-    for(auto& x : m)
-    {
-        fmt::printf(fmt::format(FMT_STRING("key:{:s}\n"),x.first));
-    }
-    std::cout << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    fmt::print("time to fill map:{}\n",diff.count());
+    
+    
     fmt::printf(fmt::format(FMT_STRING("size:{:d}\n"),m.size()));
 
+    auto start1 = std::chrono::high_resolution_clock::now();
     for(const auto& s : v)
     {
         const auto p = m.find(s);
         if(p != m.end());
         {
-            fmt::printf(fmt::format(FMT_STRING("key:{:s}\n"),p->first));
+            if(p->first.length() != 50)            
+                fmt::printf(fmt::format(FMT_STRING("key:{:s}\n"),p->first));
 
         }
 
     }
+
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff1 = end1-start1;
+    fmt::print("time to search map:{}\n",diff1.count());
 
     return 0;
 }
